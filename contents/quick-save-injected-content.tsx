22c11d1f3f9d441ca2a3a52f8ebda83251c06da8
Zoran <emailzed03@gmail.com>
2023-11-25 10:13:09 +1100

    a better name

--- contents/quick-save-injected-content.tsx
+++ contents/quick-save-injected-content.tsx
@@ -1,7 +1,7 @@
 import type { PlasmoCSConfig } from "plasmo";
 import { useMessage } from "@plasmohq/messaging/hook";
 import type { PageData } from "~PageData";
-import { MessageConstants } from "~message-constants";
+import { MessageConstants, PrefillSupportedPagesUrlArray } from "~constants";
 import toast, { Toaster, type Toast } from "react-hot-toast";
 
 import styleText from "data-text:../style.css"
@@ -11,8 +11,7 @@ import { ApiUrl } from "~config";
 
 
 export const config: PlasmoCSConfig = {
-  // todo specify exact urls
-  matches: ["<all_urls>"],
+  matches: PrefillSupportedPagesUrlArray,
 }
 
 // exports the tailwind css styles
@@ -21,6 +20,7 @@ export const getStyle: PlasmoGetStyle = () => {
   style.textContent = styleText
   return style
 }
+
 // todo: export to separate components
 const successIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-1 17l-5-5.299 1.399-1.43 3.574 3.736 6.572-7.007 1.455 1.403-8 8.597z"/></svg>
 const errorIcon = () => <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" width="24" height="24" strokeMiterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m12.002 2.005c5.518 0 9.998 4.48 9.998 9.997 0 5.518-4.48 9.998-9.998 9.998-5.517 0-9.997-4.48-9.997-9.998 0-5.517 4.48-9.997 9.997-9.997zm0 8.933-2.721-2.722c-.146-.146-.339-.219-.531-.219-.404 0-.75.324-.75.749 0 .193.073.384.219.531l2.722 2.722-2.728 2.728c-.147.147-.22.34-.22.531 0 .427.35.75.751.75.192 0 .384-.073.53-.219l2.728-2.728 2.729 2.728c.146.146.338.219.53.219.401 0 .75-.323.75-.75 0-.191-.073-.384-.22-.531l-2.727-2.728 2.717-2.717c.146-.147.219-.338.219-.531 0-.425-.346-.75-.75-.75-.192 0-.385.073-.531.22z" fillRule="nonzero"/></svg>
