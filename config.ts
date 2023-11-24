25502e77ca8db39a4fceb479184d0e7d3be2fc26
Zoran <emailzed03@gmail.com>
2023-11-24 14:31:32 +1100

    fixed dev api route

--- config.ts
+++ config.ts
@@ -1,3 +1,5 @@
 
-
-export const ApiUrl: string = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "";
+/**
+ * API URL
+ */
+export const ApiUrl: string = process.env.NODE_ENV == "development" ? "http://localhost:3000/api" : "";
