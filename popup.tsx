72c198146eeec1ba1d38f14571501fd451bd98fc
Zoran <emailzed03@gmail.com>
2023-12-01 11:04:37 +1100

    Update popup.tsx

--- popup.tsx
+++ popup.tsx
@@ -116,6 +116,7 @@ function IndexPopup() {
          className="rounded-lg hover:bg-indigo-500 bg-indigo-400 text-white font-bold p-2 flex justify-center items-center"
         >
           { isSaving ? 
+            // todo: export to component
               <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -163,13 +164,13 @@ function IndexPopup() {
          Options Panel
       </button>
     </div>
-    <div className="w-full flex justify-center mt-2">
+    <div className="w-full flex justify-center mt-2 ">
       <a 
         target="_blank"
         rel="noreferrer"
-        href="https://github.com/Zed-Bailey/job-saver" className="hover:underline text-sm"
+        href="https://github.com/Zed-Bailey/job-saver" className="hover:underline text-xs text-gray-300 hover:text-gray-800"
       >
-        Project Github
+        Extension Github
       </a>
     </div>
       
