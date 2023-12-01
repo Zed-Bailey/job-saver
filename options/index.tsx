73a43d7219f029a35c3baf3d1606d87a3d0b98d0
Zoran <emailzed03@gmail.com>
2023-12-01 11:04:32 +1100

    loading indicator

--- options/index.tsx
+++ options/index.tsx
@@ -34,6 +34,8 @@ function OptionsIndex() {
     const [formState, setFormState] = useState<OptionsFormState>({
         sheetUrl: ""
     });
+    const [isLoading, setIsLoading] = useState(false);
+
 
     function parseUrl(urlString: string) {
         
@@ -92,6 +94,7 @@ function OptionsIndex() {
 
     const saveChanges = async (e : FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setIsLoading(true);
         const body = {
             url: formState.sheetUrl
         };
@@ -115,6 +118,7 @@ function OptionsIndex() {
             toast.error("Failed to validate google sheet");
             setValidationMessage({error: json.error, success: null});
         }
+        setIsLoading(false);
     }
 
     return(
@@ -145,8 +149,15 @@ function OptionsIndex() {
                         </div>
                         
                     
-                        <button type="submit" className="bg-indigo-400 py-2 font-medium rounded-lg mt-3 hover:bg-indigo-600 hover:text-white">
-                            Connect To Sheet
+                        <button type="submit" className="bg-indigo-500 py-2 font-medium rounded-lg mt-3 hover:bg-indigo-600 text-white flex justify-center items-center" disabled={isLoading}>
+                            {isLoading ?
+                                <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                : null
+                            }
+                            {isLoading ? "Connecting..." : "Connect To Sheet"}
                         </button>
                         <div>
                             {validationMessage.error != null ? <p className="text-red-600">{validationMessage.error}</p> : null}
